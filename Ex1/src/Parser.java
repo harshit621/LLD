d6a7cee9f4df2951cf193a514d963e4793130692
harshit621 <harshitsen199@gmail.com>
2026-03-05 23:55:29 +0530

    Add files via upload

---
+++ Ex1/src/Parser.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Parser {
+    public Student parse(String raw) {
+        System.out.println("INPUT: " + raw);
+
+        Map<String,String> kv = new LinkedHashMap<>();
+        String[] parts = raw.split(";");
+        for (String p : parts) {
+            String[] t = p.split("=", 2);
+            if (t.length == 2) kv.put(t[0].trim(), t[1].trim());
+        }
+
+        String name = kv.getOrDefault("name", "");
+        String email = kv.getOrDefault("email", "");
+        String phone = kv.getOrDefault("phone", "");
+        String program = kv.getOrDefault("program", "");
+
+        Student newStd = new Student(name, email, phone, program);
+        return newStd;
+    }
+
+
+
+
+
+    
+
+}
